8733de05b21c4b8ca2e76a1ca70dbd33a81fd8e0
HYxiaojian <165478586+hyheiyue@users.noreply.github.com>
2025-01-22 00:45:56 +0800

    Update rm_sentry_simulation_launch.py
    Update rm_sentry_simulation_launch.py

--- bringup_nav/launch/rm_sentry_simulation_launch.py
+++ bringup_nav/launch/rm_sentry_simulation_launch.py
@@ -93,7 +93,7 @@ def generate_launch_description():
     declare_params_file_cmd = DeclareLaunchArgument(
         "params_file",
         default_value=os.path.join(
-            bringup_dir, "config","simulation" "nav2_params.yaml"
+            bringup_dir, "config","simulation","nav2_params.yaml"
         ),
         description="Full path to the ROS2 parameters file to use for all launched nodes",
     )
